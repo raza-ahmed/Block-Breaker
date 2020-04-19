91c9b93b5618771ba95cc988a4efb16818ab3d0b
Ahmed Raza <raza2393@gmail.com>
2020-04-19 16:05:33 +0530

    Ball Colliding with rectangle

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    static MusicPlayer instance = null;
+
+    void Awake()
+    {
+        Debug.Log("Music Player is Awake" +GetInstanceID());
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Music Player is Start" + GetInstanceID());
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+      // Debug.Log("Music Player is Update" + GetInstanceID());
+    }
+}
